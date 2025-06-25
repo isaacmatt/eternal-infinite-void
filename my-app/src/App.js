c74a2a799e8833065e5511b0a9ec42fe0d2fe8e9
isaacmatt <114306353+isaacmatt@users.noreply.github.com>
2025-06-25 12:09:27 -0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <div className="blackhole"></div>
       <h1>Welcome to my portfolio!</h1>
+      <h2>Thanks for visiting!</h2>
       <div className="buttons">
         <button onClick={() => alert('About Me: Coming Soon!')}>About Me</button>
         <button onClick={() => alert('Other Items: Coming Soon!')}>Other Items</button>
